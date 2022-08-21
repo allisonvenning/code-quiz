5fc1c9403906b57713348e4c95f94494347ef2bd
Allison Venning <allisonvenning7@gmail.com>
2022-08-20 20:49:28 -0400

    added questions in script.js, start quiz function

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+// var buttonEl = document.querySelector("#start-quiz");
+// console.log(buttonEl);
+
+// buttonEl.addEventListener("click", function() {
+//     var switchScreen = 
+// })
+
+const startButton = document.getElementById('start-quiz')
+const questionScreenElement = document.getElementById('question-screen')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz() {
+    console.log('Started')
+    startButton.classList.add('hide')
+    questionScreenElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [ {
+    question: "True or False: Functions can be passed as arguments to other functionsl",
+    answers: [
+        {text: "true", correct: true },
+        {text: "false", correct: false}
+    ]
+},
+{
+    question: "Which operator is used to represent AND statments?",
+    answers: [
+        {text: "&&", correct: true},
+        {text: "||", correct: false},
+        {text: "+", correct: false},
+        {text: "&", correct: false}
+    ]
+},
+{
+    question: "Which of the following is an example of an array?",
+    answers: [
+        {text: "var array = {}", correct: false },
+        {text: "var array = ()", correct: false },
+        {text: "var array = []", correct: true },
+        {text: "var array = <>", correct: false }
+    ]
+}
+
+]
